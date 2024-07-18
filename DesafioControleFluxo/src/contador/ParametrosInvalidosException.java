515fd935f0ddb9f36bb9b9de0e8a43267c286379
Ariel Soares <arielsoares622@gmail.com>
2024-07-18 01:30:01 -0300

    remove

--- DesafioControleFluxo/src/contador/ParametrosInvalidosException.java
+++
@@ -1,9 +0,0 @@
-package contador;
-
-public class ParametrosInvalidosException extends Exception{
-	public ParametrosInvalidosException(String message) {
-		super(message);
-	}
-	private static final long serialVersionUID = 1L;
-
-}
--- DesafioControleFluxo/src/contador/ValidadorParametro.java
+++
@@ -1,8 +0,0 @@
-package contador;
-
-public class ValidadorParametro {
-	public void validar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-		if(parametroUm >= parametroDois)
-			throw new ParametrosInvalidosException("Erro! O valor do SEGUNDO parâmetro deve ser maior que o primeiro.");
-	}
-}
